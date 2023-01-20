ebe993fc6ccd5a90080b7b6bf051294be056c023
Cap3ta02 <123204068+Cap3ta02@users.noreply.github.com>
2023-01-20 21:00:50 +0100

    Update primocontratto.sol

--- primocontratto.sol
+++ primocontratto.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.8.17;
 
 contract primocontratto{
     string public primaStringa = "Ciao a tutti";
+    
+    function arrayDinamico()public view return (string primopagamento)
+        {
+        return pagamento effettuato
+        }
 }
