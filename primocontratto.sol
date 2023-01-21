e8589aa8c31cb0deb776850f55d59c5f53075f8f
Cap3ta02 <123204068+Cap3ta02@users.noreply.github.com>
2023-01-21 10:07:20 +0100

    Update primocontratto.sol

--- primocontratto.sol
+++ primocontratto.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.17;
 
 contract primocontratto{
 
+    uint[1] public numeroTotalePagamenti = [5];
+    function arrayDinamico()public view returns (string primopagamento){
+        return contrattoCollaborazioneAttilio;
+    }
+    function aggiornaarray (uint _number) public {
+        arraydinamiconumerico.push(_number);
+    } 
+
+    function contapagamenti () public view returns (uint){
+        return arraydinamiconumerico.leght;
     
-    function arrayDinamico()public view return (string primopagamento)
-        {
-        return pagamento effettuato
-        }
+    }
 }
