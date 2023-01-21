3337bacd99eb24d0f62e0a5f273842e8fe2afdc7
Cap3ta02 <123204068+Cap3ta02@users.noreply.github.com>
2023-01-21 13:32:11 +0100

    Update primocontratto.sol

--- primocontratto.sol
+++ primocontratto.sol
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract primocontratto{
 
-    uint[1] public numeroTotalePagamenti = [5];
-    function arrayDinamico()public view returns (string primopagamento){
-        return contrattoCollaborazioneAttilio;
-    }
+contract primocontratto{
+    
+    string [1] public appartenenza = ["contratto appartenenza attilio ferrise"] ;
+    
+    string [1] private conteggiopagamenti = ["5 pagamenti da completare"];
+
+    uint [] private arraydinamiconumerico = [0];
+
+
     function aggiornaarray (uint _number) public {
         arraydinamiconumerico.push(_number);
+        
     } 
 
     function contapagamenti () public view returns (uint){
-        return arraydinamiconumerico.leght;
-    
+        return arraydinamiconumerico.length;
+        
     }
 }
