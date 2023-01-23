b2b658d383aded3135aa262f5c33d32abb785560
Cap3ta02 <123204068+Cap3ta02@users.noreply.github.com>
2023-01-23 20:15:00 +0100

    Update primocontratto.sol

--- primocontratto.sol
+++ primocontratto.sol
@@ -14,10 +14,22 @@ contract primocontratto{
     function aggiornaarray (uint _number) public {
         arraydinamiconumerico.push(_number);
         
-    } 
-
+    }
     function contapagamenti () public view returns (uint){
         return arraydinamiconumerico.length;
         
     }
-}
+} 
+    struct pagamento {
+        string proprietario;
+        string Data_pagamento;
+        uint totale_pagamento;
+        }
+    mapping (uint => pagamento ) public variabile;
+
+    function addAggiornamenti (uint _index,string memory _nome, string memory _data, uint  _totale) public {
+
+        variabile [_index] = pagamento (_nome, _data, _totale);
+    }
+}   
+    
