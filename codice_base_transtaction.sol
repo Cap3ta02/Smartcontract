3dc86306de21dc1719eece7d21c4891e1ab87897
Cap3ta02 <123204068+Cap3ta02@users.noreply.github.com>
2023-01-28 18:19:24 +0100

    Update codice_base_transtaction.sol

--- codice_base_transtaction.sol
+++ codice_base_transtaction.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+contract transizione{
+
+    address indirizzo;
+
+    function setIndirizzo() public payable{
+        indirizzo = msg.sender;
+    }
+
+    function getIndirizzo () public view returns (address) {
+        return indirizzo;
+
+    }
+
+    function getbalance () public view returns (uint){
+        return indirizzo.balance;
+    }
+
+    function tasferimentoEth (address payable _address, uint _value ) public payable {
+        _address.transfer (_value) ;
+
+    }
+}
