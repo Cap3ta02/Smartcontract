e4da8b6635caee450c6335e9d33d43bd61a22e18
Cap3ta02 <123204068+Cap3ta02@users.noreply.github.com>
2023-01-28 16:39:26 +0100

    Update codici_in_riga.sol

--- codici_in_riga.sol
+++ codici_in_riga.sol
@@ -44,5 +44,14 @@ contract primocontratto{
         else{
             returne false;
         }
-        }
     }
+}
+
+//____________________________________________________________
+mapping (uint => string) public persona; /* andiamo a definire che attraverso un intero richiamiamo un stringa */
+                                    /* con persona si definisce il nome del mapping */
+    costructor () {
+        persona[0] = "Persona Uno"
+        persona[1] = "Persona Due"
+    }
+}
