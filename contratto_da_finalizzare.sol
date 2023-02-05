e96396d37323c62634be5c7fb4064f40e1824898
Cap3ta02 <123204068+Cap3ta02@users.noreply.github.com>
2023-02-05 15:09:23 +0100

    Update contratto_da_finalizzare.sol

--- contratto_da_finalizzare.sol
+++ contratto_da_finalizzare.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.18;
 
 contract primocontratto{
-    string [1] public appartenenza = ["contratto appartenenza :nomedelproprietario:"];:
+    string [1] public appartenenza = ["contratto appartenenza :nomedelproprietario:"];
 
     string [1] private conteggiopagamenti = ["totale pagamenti da cmompletare = 5"];
 
     uint [] private arrayconteggiopagamenti = [0];
 
+    uint contatore;
+
     function aggiornaarray (uint _inputpagamento ) public {
 
-        arrayconteggiopagamenti.push (_inputpagamento) ;
+        arrayconteggiopagamenti.push ( _inputpagamento ) ;
 
     }
 
     function conteggio_totale_dei_pagamenti () public view return  (uint){
+        
         return arrayconteggiopagamenti.length;
 
     }
@@ -27,10 +30,16 @@ contract primocontratto{
 
     mapping ( uint => pagamento ) public variabile;
 
-    function addPagamento ( uint _index, string memory _data, uint _importo) public {
+    function addPagamento ( uint _index, string memory _data, uint _importo, ) public {
         variabile [_index] = pagamento  (_data, _importo);
+        
+        contatore ++;
+
+    return contatore;
+
 
     }
+
 
     
 
